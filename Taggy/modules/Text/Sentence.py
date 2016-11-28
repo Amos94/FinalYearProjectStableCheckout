66593d272f58aa4936d29fc9c02bb87ea084e018
amosmadalinneculau <amos.neculau@kcl.ac.uk>
2016-11-28 14:26:17 +0000

    Completed User class Completed Sentence class Started Post class

---
+++ Taggy/modules/Text/Sentence.py
@@ -0,0 +1,60 @@
+class Sentence():
+    """
+    * id of containing post
+    *
+    * @var int
+    """
+    __postId = 0
+
+    """
+    * id of sentence
+    *
+    * @var int
+    """
+    __sentenceID = 0
+
+    """
+    * display ready text of sentence
+    *
+    * @var string
+    """
+    __sentence = ""
+
+    """
+    * number of paragraph which sentence belongs to (count starts with 1)
+    *
+    * @var int
+    """
+    __paragraphInPost = 0
+
+    """
+    * number of sentence within paragraph (count starts with 1)
+    *
+    * @var int
+    """
+    __sentenceInParagraph = 0
+
+    """
+    Sentence Constructor
+    """
+    def __init__(self, postId, sentenceId, sentence, paragraphInPost, sentenceInParagraph):
+        self.__postId = postId
+        self.__sentenceID = sentenceId
+        self.__sentence = sentence
+        self.__paragraphInPost = paragraphInPost
+        self.__sentenceInParagraph = sentenceInParagraph
+
+    def getPostId(self):
+        return self.__postId
+
+    def getSentenceId(self):
+        return self.__sentenceID
+
+    def getSentence(self):
+        return self.__sentence
+
+    def getParagraphInPost(self):
+        return self.__paragraphInPost
+
+    def getSentenceInParagraph(self):
+        return self.__sentenceInParagraph
--- Taggy/modules/users/User.py
+++ Taggy/modules/users/User.py
@@ -1,11 +1,85 @@
 
 class User():
 
-    def getUser(self):
-        return "amos"
+    """
+    private parameters
 
-    def getSessionId(self):
-        return '0'
+    username - username of the user registered
+    userId - id of the user registered
+    userType - type of the user registered.
+        user types: Admin, Annotator, Adjudecator [TO BE EXTENDED WITH PLUGIN]
 
-    def isAuthenticated(self):
-        return True
+    """
+    _username = ""
+    _userId = ""
+    _userType = ""
+
+    """
+    Constructor
+    """
+    def __init__(self, username, userId, userType):
+        self.__username = username
+        self.__userId = userId
+        self.__userType = userType
+
+    """
+    Function:
+        Returns the username
+    """
+    def getUsername(self):
+        return self.__username
+
+    """
+    Function:
+        Returns the userId
+    """
+    def getUserId(self):
+        return self.__userId
+
+    """
+    Function:
+        Returns the userType
+    """
+    def getUserType(self):
+        return self.__userType
+
+    """
+    Function:
+        Sets the username.
+        Useful when you want to change a username.
+
+    TO ADD DATABASE UPDATE
+    """
+    def setUsername(self, username):
+
+        oldUsername = self.__username
+        self.__username = username
+        return oldUsername
+
+    """
+    Function:
+        Sets the username.
+        Useful when you want to change a type.*maybe from annotator to adjudicator*
+
+    TO ADD DATABASE UPDATE
+    """
+    def setUserType(self, userType):
+
+        oldUserType = self.__userType
+        self._userType = userType
+        return oldUserType
+
+
+    def isAdmin(self):
+
+        toReturn = False
+        if(self.__userType == "ADMIN"):
+            toReturn = True
+        return toReturn
+
+    def canAdjudicate(self):
+
+        toReturn = False
+        if(self.__userType == "ADMIN" or self.__userType == "ADJUD"):
+            toReturn = True
+        return toReturn
