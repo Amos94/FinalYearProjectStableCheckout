8b9b3619b7310a8ff9be32a8862956ba8c1723c3
amosmadalinneculau <amos.neculau@kcl.ac.uk>
2016-11-30 12:55:39 +0000

    Creating the dadabase in the Models.

--- Taggy/models.py
+++ Taggy/models.py
@@ -4,10 +4,60 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class annotators(models.Model):
 
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
+    annotatorId = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=32)
+    usertype = models.CharField(max_length=10)
+
+
+
+
+class annotators_sets(models.Model):
+
+    setId = models.AutoField(primary_key=True)
+    annotatorId = models.IntegerField()
+
+
+
+
+class categories(models.Model):
+
+    categoryId = models.AutoField(primary_key=True)
+    catName = models.CharField(max_length=100)
+
+
+
+
+class chack_posts(models.Model):
+
+    postId = models.IntegerField()
+    forumId = models.IntegerField()
+    topicId = models.IntegerField()
+    creationDate = models.DateField()
+    profileId = models.IntegerField()
+    authorName = models.TextField()
+
+
+
+
+class emoticons(models.Model):
+
+    emoticonId = models.AutoField(primary_key=True)
+    canonicalForm = models.TextField()
+
+
+
+class emoticons_tag(models.Model):
+
+    tagId = models.AutoField(primary_key=True)
+    emoticonId = models.IntegerField()
+    rawTag = models.IntegerField()
+
+
+
+class forums(models.Model):
+
+    forumId = models.IntegerField()
+    forumName = models.CharField(max_length=100)
