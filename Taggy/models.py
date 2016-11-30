7e93712fc507d87ecfea521396cfc6cf97506001
amosmadalinneculau <amos.neculau@kcl.ac.uk>
2016-11-30 13:47:37 +0000

    Finished adding classes in Models.

--- Taggy/models.py
+++ Taggy/models.py
@@ -139,4 +139,60 @@ class sentences(models.Model):
     sentenceId = models.AutoField(primary_key=True)
     postId = models.IntegerField()
     sentence = models.TextField()
-    
+    paragraphInPost = models.IntegerField()
+    sentenceInParagraph = models.IntegerField()
+
+
+
+
+
+class sentences_tags(models.Model):
+
+    sentenceId = models.IntegerField()
+    tagId = models.IntegerField()
+    postId = models.IntegerField()
+    annotatorId = models.IntegerField()
+    timestamp = models.DateTimeField()
+
+
+
+
+class sets(models.Model):
+
+    setId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    description = models.TextField()
+    creatordId = models.IntegerField()
+
+
+
+
+class signatures(models.Model):
+
+    postId = models.IntegerField()
+    profileId = models.IntegerField()
+    signatures = models.TextField()
+
+
+
+
+class tags(models.Model):
+
+    tagId = models.AutoField(primary_key=True)
+    tagName = models.CharField(max_length=4)
+    tagDescription = models.CharField(max_length=250)
+    provideOrRequest = models.CharField(max_length=1)
+
+
+
+
+class topics(models.Model):
+
+    topicId = models.IntegerField()
+    url = models.CharField(max_length=25)
+    title = models.CharField(max_length=150)
+    creationDate = models.DateTimeField()
+    profileId = models.IntegerField()
+    lastDate = models.DateTimeField()
+    numViews = models.IntegerField()
+    forumId = models.IntegerField()
