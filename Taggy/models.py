687bee704d8c044401bf99dc3da7e8adf5371664
amosmadalinneculau <amos.neculau@kcl.ac.uk>
2016-11-30 13:10:51 +0000

    More added in Models.

--- Taggy/models.py
+++ Taggy/models.py
@@ -35,7 +35,7 @@ class chack_posts(models.Model):
     postId = models.IntegerField()
     forumId = models.IntegerField()
     topicId = models.IntegerField()
-    creationDate = models.DateField()
+    creationDate = models.DateTimeField()
     profileId = models.IntegerField()
     authorName = models.TextField()
 
@@ -61,3 +61,39 @@ class forums(models.Model):
 
     forumId = models.IntegerField()
     forumName = models.CharField(max_length=100)
+    forumDescription = models.CharField(max_length=250)
+    categoryId = models.IntegerField()
+
+
+
+
+
+class posts(models.Model):
+
+    postId  = models.AutoField(primary_key=True)
+    forumId = models.IntegerField()
+    topicId = models.IntegerField()
+    creationDate = models.DateTimeField()
+    profileId = models.IntegerField()
+    content = models.TextField()
+
+
+
+
+class posts_annotators(models.Model):
+
+    annotatorId = models.IntegerField()
+    postId = models.IntegerField()
+    numSentencesInPost = models.IntegerField()
+    comment = models.TextField()
+    numSentencesTagged = models.IntegerField()
+    lastUpdated = models.DateTimeField()
+
+
+
+
+class posts_emoticons(models.Model):
+
+    postId = models.IntegerField()
+    emoticonId = models.IntegerField()
+    tagId = models.IntegerField()
