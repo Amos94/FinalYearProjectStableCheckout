be4f71f2e07ad0d2d8ce1fd56eb07ff690e15e33
Amos Neculau <amos.neculau@kcl.ac.uk>
2017-01-22 21:10:28 +0000

    more on queries

--- taggy/modules/Queries.py
+++ taggy/modules/Queries.py
@@ -45,7 +45,7 @@ class Queries:
     """
     def getAnnotatorsForPost(self, postid):
 
-        #Building the query
+        #Building the SQL query
         qry =  "SELECT TAGGY_posts_annotators.annotatorId, TAGGY_annotators.username "
         qry += "FROM TAGGY_post_annotators, TAGGY_annotators "
         qry += "WHERE postId='"+postid+"' "
@@ -77,10 +77,53 @@ class Queries:
     """
     def getAnnotatorsForSet(self, setid):
 
-        qry = ""
+        # Building the SQL query
+        qry =  "SELECT taggy_annotators_sets.annotatorId, taggy_annotators.username "
+        qry += "FROM taggy_annotators_sets, taggy_annotators "
+        qry += "WHERE setId='"+setid+"' "
+        qry += "AND taggy_annotators.usertype='ANNOT_TYPE.' "
+        qry += "AND taggy_annotators_sets.annotatorId = taggy_annotators.annotatorId "
+        qry += "ORDER BY taggy_annotators_sets.annotatorId"
 
+        # execution of the query 'qry'
         with connection.cursor() as cursor:
             cursor.execute(qry)
+            # fetching all data of the query 'qry'
             qryResult = cursor.fetchall()
 
+        # return the data
+        return qryResult
+
+
+
+
+
+
+    """
+   * getAnnotatorsProgressByPost()
+   *
+   * returns the Annotator IDs, and progress for a particular Post ID
+   *
+   * @param text $usr  username argument
+   * @param text $userid  (alternatively) query by annotatorID argument
+    """
+
+    def getAnnotatorsProgressByPost(self, postId, state=''):
+
+        # Building the SQL query
+        qry =  "SELECT annotatorId,(numSentencesInPost / numSentencesInPost) as progress, lastUpdated, postAnnotatorState as state "
+        qry += "FROM taggy_posts_annotators "
+        qry += "WHERE postId='"+postId+"' "
+
+        if(state):
+            qry += "AND postAnnotatorState='"+state+"'"
+
+
+        # execution of the query 'qry'
+        with connection.cursor() as cursor:
+            cursor.execute(qry)
+            # fetching all data of the query 'qry'
+            qryResult = cursor.fetchall()
+
+        # return the data
         return qryResult
