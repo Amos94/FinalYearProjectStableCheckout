f96860305124fda89d03d2302c7c662c197420b7
Amos Neculau <amos.neculau@kcl.ac.uk>
2017-01-26 21:12:16 +0000

    changed the private variables to public variables AnnotatedPost - finished

--- taggy/modules/post/AnnotatedPost.py
+++ taggy/modules/post/AnnotatedPost.py
@@ -16,28 +16,28 @@ class AnnotatedPost(Post):
     *
     * @var TagLookupTable
     """
-    __lookup = None
+    lookup = None
 
     """
     * Annotator Object
     *
     * @var Annotator
     """
-    __annotator = None
+    annotator = None
 
     """
     * Annotator Object
     *
     * @var Annotator
     """
-    __annotator_states = []
+    annotator_states = []
 
     """
     * comments for this post indexed by Annotator ID
     *
     * @var array
     """
-    __comments = []
+    comments = []
 
     """
     * Constructor of AnnotatedPost object
@@ -47,17 +47,93 @@ class AnnotatedPost(Post):
     """
     def __init__(self, queryObject, postId = -1, annotator):
 
-        self.__lookup = TagLookUpTable(queryObject)
-        self.__annotator = annotator
+        self.lookup = TagLookUpTable(queryObject)
+        self.annotator = annotator
 
         # Get current PostState and Comment
         postAnnotation = self.execQuery(queryObject.getPostAnnotation(postId, annotator.getId()))
-        self.__annotator_states[annotator.getId()] = postAnnotation['postAnnotatorState']
-        self.__comments[annotator.getId()] = postAnnotation['comment']
+        self.annotator_states[annotator.getId()] = postAnnotation['postAnnotatorState']
+        self.comments[annotator.getId()] = postAnnotation['comment']
 
         Post.__init__(queryObject, postId)
 
 
+    """
+    * addSentence()
+    *
+    * adds a sentence to the post's collection of sentences.  Assumes that sentences
+    * are added in display order.  No sorting is done prior to display.
+    *
+    * @return void
+    """
+    def addSentence(self, qryObject, args):
+        self.sentences.append(AnnotatedSentence( qryObject, self.lookup, self.annotator, args ))
+
+
+    def render_table_header(self):
+        print("<tr><th width='5%'></th><th width='65%'>sentence</th><th width='30%'>tags</th></tr>")
+
+    """
+    * render_finalize_button()
+    *
+    * renders HTML for appropriated finalize button
+    *
+    """
+    def render_finalize_button(self, qryObject):
+
+        if(self.annotator_states[self.annotator.id] == 'DONE' or self.annotator_states[self.annotator.id] == 'ADJUDICATED'):
+            finalizeHiddenCls = "initiallyHidden"
+            unfinalizeHiddenCls = ""
+        else:
+            finalizeHiddenCls = ""
+            unfinalizeHiddenCls = "initiallyHidden"
+
+        if(self.postState == 'ADJUDICATED'):
+            finalizedDisabledCls = "finalizedDisabled"
+        else:
+            finalizedDisabledCls = "finalizedEnabled"
+
+        print( "<div id='finalize' class='postBtn finalizePostBtn "+
+          finalizeHiddenCls+" "+
+          +finalizedDisabledCls+"'>FINALIZE</div>\n")
+
+        print("<div id='unfinalize' class='postBtn finalizePostBtn "+
+          unfinalizeHiddenCls+" "+
+          finalizedDisabledCls+"'>--DONE--</div>\n")
+
+    """
+    * render_posts_annotation()
+    *
+    * renders HTML for populated Comment section for this post and a particular annotator
+    *
+    * @param Queries $qryObject a DB Queries object
+    * @return void
+    """
+    def render_posts_annotation(self, qryObject):
+        print("<div id='annotatorsCommentContainer'>\n")
+        print("<i>")
+        print("Comments:")
+        print("</i>\n")
+        print("<textarea id='annotatorsComment'>\n")
+        print( self.comments[self.annotator.id])
+        print("</textarea>\n")
+        print("<div id='annotatorsCommentSave' class='postBtn saveCommentsBtn '>SAVE COMMENTS</div>")
+        print("</div>\n")
+        print("<p></p>\n")
+
+    """
+    * render_available_tags()
+    *
+    * renders HTML available tags for is this post
+    *
+    * @return void
+    """
+    def render_available_tags(self):
+        self.lookup.render()
+
+    """
+    MYSQL QUERY EXECUTER CLASS
+    """
     def execQuery(self, qry):
         # execution of the query 'qry'
         with connection.cursor() as cursor:
--- taggy/modules/post/Post.py
+++ taggy/modules/post/Post.py
@@ -3,11 +3,11 @@ import taggy.modules.Queries as Queries
 
 class Post():
 
-    __postId = 0
-    __postError = 0
-    __forumId = 0
-    __topicId = 0
-    __sentences = []
+    postId = 0
+    postError = 0
+    forumId = 0
+    topicId = 0
+    sentences = []
 
     def __init__(self, qryObject, postId = -1):
 
@@ -20,46 +20,46 @@ class Post():
             results = qryObject.getPost(postId)
 
         if(results.count != 1):
-            self.__postId = postId
-            self.__postError = 'PostID ' + postId + ' does not exist or is not ready to be annotated.'
+            self.postId = postId
+            self.postError = 'PostID ' + postId + ' does not exist or is not ready to be annotated.'
 
         else:
             rows = results.fetchall()
 
             for row in rows:
-                self.__postId = row['postID']
-                self.__forumId = row['forumID']
-                self.__topicId = row['topicID']
-                self.__postState = row['postState']
+                self.postId = row['postID']
+                self.forumId = row['forumID']
+                self.topicId = row['topicID']
+                self.postState = row['postState']
 
-            results = qryObject.getSentences(self.__postId)
+            results = qryObject.getSentences(self.postId)
             rows = results.fetchall()
 
             for row in rows:
                 self.addSentence(qryObject, row)
 
-            self.__postError = None
+            self.postError = None
 
 
 
 
     def getPostId(self):
-        return self.__postId
+        return self.postId
 
     def getPostError(self):
-        return self.__postError
+        return self.postError
 
     def getForumId(self):
-        return self.__forumId
+        return self.forumId
 
     def getTopicId(self):
-        return self.__topicId
+        return self.topicId
 
     def getSentences(self):
-        return self.__sentences
+        return self.sentences
 
     def addSentence(self, qryObkect, args):
-        self.__sentences.append(args)
+        self.sentences.append(args)
 
     """
     * render_headline()
