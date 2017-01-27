2eba3679d41116106aa40aa1ddd673e74b276687
Amos Neculau <amos.neculau@kcl.ac.uk>
2017-01-27 15:38:57 +0000

    AnnotatedSentence - added and questions about args in constructor

---
+++ taggy/modules/sentence/AnnotatedSentence.py
@@ -0,0 +1,64 @@
+from taggy.modules.Queries import Queries
+from taggy.modules.sentence.Sentence import Sentence
+
+def AnnotatedSentence(Sentence):
+
+    lookup = None
+    annotator = None
+    """
+    * array of tagIds as integers associated with this Sentence (differs based upon annotator)
+    *
+    """
+    tags = []
+
+
+
+
+    """
+    * Constructor of Sentence Object
+    *
+    """
+    def __init__(self, qryObject, lookup, annotator, args):
+
+        self.lookup = lookup
+        self.annotator = annotator
+
+        Sentence.__init__(args)
+
+        self.tags[annotator.id] = self.retrieve_tags(qryObject, annotator)
+
+
+
+    """
+    * populates the tagIds array for a particular post and annotator (is post id necessary in query?)
+    *
+    * @param Queries $qryObject the DB Queries object
+    * @param int $annotatorID id for annotator
+    * @return void
+    """
+    def retrieve_tags(self, qryObject, a):
+        qryObject = Queries
+
+        results = qryObject.getSentenceTags(self.postId, self.sentenceId, a.id)
+
+        t = []
+
+        for row in results:
+            t.append(self.lookup.tag(row['tagID']))
+
+        return t
+
+
+
+    """
+    * renders <td> columns for tags
+    *
+    * @return void
+    """
+    def render_tag_columns(self):
+        print("<td class='tagsSentence'>")
+
+        for(t in self.tags[self.annotator.id]):
+            t.render_as_div()
+
+        print("</td>")
