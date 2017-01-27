5f57a064765f0eb05d3741eda91ec7d28bb022d8
Amos Neculau <amos.neculau@kcl.ac.uk>
2017-01-27 15:04:50 +0000

    Adjudicated Post - finished

--- taggy/modules/post/AdjudicatedPost.py
+++ taggy/modules/post/AdjudicatedPost.py
@@ -61,3 +61,73 @@ class AdjudicatedPost(AnnotatedPost):
     * are added in display order.  No sorting is done prior to display.
     *
     """
+    def addSentence(self, qryObject, args):
+        self.sentences.append(AdjudicatedSentence( qryObject,
+                                                    self.annotatorStates[self.annotator.id],
+                                                    self.lookup,
+                                                    self.annotator,
+                                                    self.others,
+                                                    args)
+                              )
+
+
+
+    def render_table_header(self):
+        print("<tr><th width='5%'></th><th width='65%'>sentence</th>")
+        for a in self.others:
+            print("<th class='"+self.annotatorStates[a.id] +"'>tags ("+a.username+")</th>")
+        print("<th class='"+ self.annotatorStates[self.annotator.id] +"'>tags (adjudicated)</th>")
+        print("</tr>")
+
+
+
+    """
+    * render_finalize_button()
+    *
+    * renders HTML for appropriated finalize button
+    *
+    """
+    def render_finalize_button(self,qryObject):
+        if((self.annotatorStates[self.annotator.id] == 'DONE') or (self.annotatorStates[self.annotator.id] == 'ADJUDICATED')):
+            finalizeHiddenCls = "initiallyHidden"
+            unfinalizeHiddenCls = ""
+        else:
+            finalizeHiddenCls = ""
+            unfinalizeHiddenCls = "initiallyHidden"
+
+        if(self.postState == 'ADJUDICATED'):
+            finalizedDisabledCls = "finalizedDisabled"
+        else:
+            finalizedDisabledCls = "finalizedEnabled"
+
+        print("<div id='finalize' class='postBtn finalizePostBtn adjudicatorPostBtn "+finalizeHiddenCls+" "+finalizedDisabledCls+"'>FINALIZE</div>\n")
+        print("<div id='unfinalize' class='postBtn finalizePostBtn adjudicatorPostBtn "+unfinalizeHiddenCls+" "+unfinalizedDisabledCls+"'>--DONE--</div>\n")
+
+    """
+    * render_posts_annotation()
+    *
+    * renders HTML for populated Comment section for this post and a particular annotator
+    *
+    * @param Queries $qryObject a DB Queries object
+    * @return void
+    """
+    def render_posts_annotation(self, qryObject):
+        self.render_posts_annotation(qryObject)
+
+        print("<b><span id='othersCommentsLabel'>Other Annotators Comments: <span id='othersCommentsDisplay'>show/hide</span></span></b>")
+        print("<div id='othersComments'>\n")
+        print("<table>\n")
+
+        for a in self.others:
+            print("<tr>")
+            print("<td class='"+ self.annotatorStates[a.id] +"'>"+a.username+"</td>")
+
+            if(self.comments[a.id]):
+                print("<td>" + self.comments[a.id] + "</td>")
+            else:
+                print("<td><i>no comments</i></td>")
+
+            print("</tr>\n")
+
+        print("</table>\n")
+        print("</div>\n")
