2581d182abe2126287dc6fa0482bc71bc2a2ede1
Amos Neculau <amos.neculau@kcl.ac.uk>
2017-01-28 01:09:27 +0000

    Tag - ERRORS IN render_as_div function and strange POVIDES_CODE and REQUEST_CODE usage.

--- taggy/modules/tag/Tag.py
+++ taggy/modules/tag/Tag.py
@@ -15,3 +15,27 @@ class Tag:
     def __toString(self):
         return self.name + "_" + self.provideOrRequest
 
+    def render_in_table(self):
+        print("[" + self.name + "(" + self.provideOrRequest+ ")]")
+
+    def render_as_div(self, is_tagBtn = False, is_editable = True ):
+        //
+        # CODE TO BE REVIEWED BECAUSE OF STRANGE ? "" :
+        //
+
+        if(is_tagBtn):
+            kind = 'tagBtn'
+        else:
+            kind = 'tagInstance'
+
+        tag_enumerated_class = "tag" + self.enum
+        tag_provideOrRequest_class = "tag"+ self.provideOrRequest
+        tag_enumerated_provideOrRequest_class = "tag"+ self.provideOrRequest+ self.enum
+
+        print("<div id='t"+ self.tagID+"' class='tag "+ kind+" "+is_editable +" "+tag_enumerated_class+" "+tag_provideOrRequest_class+" "+tag_enumerated_provideOrRequest_class+" '>")
+
+    def is_provides(self):
+        return self.provideOrRequest == PROVIDES_CODE
+
+    def is_request(self):
+        return self.provideOrRequest == REQUEST_CODE
