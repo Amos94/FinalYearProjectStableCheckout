39a84dc1ba12c41558c5d6e5ead317f547fbb2da
Amos Neculau <amos.neculau@kcl.ac.uk>
2017-01-30 17:24:46 +0000

    Annotator - added and finished

---
+++ taggy/modules/Annotator.py
@@ -0,0 +1,55 @@
+from taggy.modules.Queries import Queries
+
+class Annotator:
+
+    """
+    * id of user
+    *
+    * @var int
+    """
+    id = 0
+
+    """
+    * verbose system name
+    *
+    * @var string
+    """
+    username = ""
+
+    """
+    * type of user
+    *
+    * @var string
+    """
+    usertype = ""
+
+    def __init__(self, qryObject, aID):
+        """
+
+        :type qryObject: Queries
+        """
+        self.id = aID
+        results = qryObject.getAnnotators('', self.id)
+
+        if(results.count() != 1)
+            raise Exception('AnnotatorID ' + aID + ' does not exist.' )
+
+        for row in results:
+            username = row['username']
+            usertype = row['usertype']
+
+    """
+    * am I an 'admin' user?
+    *
+    * @return bool true if admin
+    """
+    def isAdmin(self):
+        return (self.usertype == "ADMIN_TYPE")
+
+    """
+    * am I an 'adjudicator' user?
+    *
+    * @return bool true if Annotator can Adjudicate
+    """
+    def canAdjudicate(self):
+        return(self.usertype == "ADJUD_TYPE" or self.usertype == "ADMIN_TYPE")
