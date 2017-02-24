7d7e07e35299187c88479c922f87ed9ec8081472
Amos Madalin Neculau <amos.neculau@kcl.ac.uk>
2017-02-24 13:22:05 +0000

    Create JsonParser
    Create JsonParser.
    
    A class that loads, iterate, and updates the database.

---
+++ taggy/JsonParser.py
@@ -0,0 +1,17 @@
+import json
+
+class JsonParser:
+
+
+"""
+LOADING THE POSTS.
+RETURN A JSON OBJECT
+"""
+def loadJSON(self, jsonString):
+  jsonObject = json.loads(jsonString)
+  
+  return jsonObject
+  
+
+
+def 
