80338bd8af374fa34d3e25f2bebd3d671ca6179e
Amos Madalin Neculau <amos.neculau@kcl.ac.uk>
2017-02-24 14:41:07 +0000

    Update JsonParser
    Update JsonParser

--- taggy/JsonParser.py
+++ taggy/JsonParser.py
@@ -1,17 +1,45 @@
 import json
+from taggy.modules.Queries import Queries
 
-class JsonParser:
+"""
+the JSON to be parsed must have the following shape:
+
+[
+for each table, the json will parse data and add/ create/ insert into a db
+]
+"""
 
 
+class JsonPostsParser:
 """
 LOADING THE POSTS.
 RETURN A JSON OBJECT
 """
-def loadJSON(self, jsonString):
+def loadPostsJSON(self, jsonString):
   jsonObject = json.loads(jsonString)
   
   return jsonObject
   
 
 
-def 
+def printPostsContent(self, jsonObject):
+  for atribute, value in jsonObject:
+    print(atribute+ " "+ value)
+    
+    
+def insertPostToDatabase(self, qryObject):
+  for atribute, value in jsonObject:
+    qryObject.insertPost(value)
+    
+"""
+{
+'posts':['post':[...],'post':[...],....]
+}
+"""
+def dumpPostsFromDatabase(self):
+  
+  posts = qry.getPosts()
+  dump = json.dumps(posts)
+  
+  return dump
+  
