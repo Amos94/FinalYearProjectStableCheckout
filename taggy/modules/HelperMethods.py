29197eaf0cf862d259143c8a8934eb6dc1693f3c
Amos Neculau <amos.neculau@kcl.ac.uk>
2017-02-28 04:49:37 +0000

    set browse added almost finished

--- taggy/modules/HelperMethods.py
+++ taggy/modules/HelperMethods.py
@@ -36,3 +36,36 @@ class HelperMethods():
         return stringToReturn
     # END OF  SET ASSIGN HELPER METHODS
     ####
+
+
+
+    ####
+    #SET BROWSE HELPER METHODS
+
+    # def renderAnnotationProgress(self, setId):
+    #     qryObject = Queries()
+    #     posts = qryObject.getSet(setId)
+    #     counter = 0
+    #     for post in posts:
+    #         counter = counter +1
+    #     total_posts = counter
+    #
+    #     results = qryObject.getAnnotatorsSets(setId)
+    #     counter = 0
+    #     for result in results:
+    #         counter = counter + 1
+    #
+    #     progress = ""
+    #     for result in results:
+    #         annotator_results = qryObject.getAnnotatorsProgressInSet( result[1], setId )
+    #         if (counter) > 0):
+    #             annotator = annotator_results
+    #             progress += annotator[1]+"("+annotator[0]+"/"+total_posts+") "
+    #         else:
+    #             annotator_results = qryObject.getAnnotators(annotator[0])
+    #             annotator = annotator_results
+    #             progress += annotator[1]+"(0/"+ total_posts+") "
+    #
+    #     return progress
+    # END OF  SET BROWSE HELPER METHODS
+    ####
